00c6efc12c5ec8637841b626cd83e2ff7911afd3
2NG CODER <nahomnigussie7@gmail.com>
2022-12-14 05:53:58 -0800

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifndef HOLBERTON_H 
-#define HOLBERTON_H
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,5 +12,3 @@ int add(int,int);
 void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
-
-#endif
