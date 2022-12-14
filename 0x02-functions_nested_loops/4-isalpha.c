dcda2530f0adcd0a8084c77a0b7b13e9515e9326
2NG CODER <nahomnigussie7@gmail.com>
2022-12-14 13:59:52 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _is alpha - Checks for alphabet is character
+ * @C: The character to be checked
+ * Return: 1 for alphabet character or 0 anything else
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 98) || (c >=97 && c <= 122))
+	{
+	return (1);
+	}
+	return (0);
+}
