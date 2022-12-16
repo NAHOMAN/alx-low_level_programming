cf5be566a13595012f429e3b556f9705e2bceb64
2NG CODER <nahomnigussie7@gmail.com>
2022-12-16 14:23:08 -0800

    isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,8 +7,8 @@ int mul(int a, int b);
 void print_number(void);
 int putchar(char);
 void print_most_number(void);
-void more_number(void)
-void print_line(int n);
+void More_number(void)
+void Print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
