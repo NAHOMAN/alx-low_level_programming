5da63aa57dca5fed5adde62176a3d509424df192
2NG CODER <nahomnigussie7@gmail.com>
2022-12-15 16:05:43 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_number(void);
+int putchar(char);
+void print_most_number(void);
+void more_number(void)
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
