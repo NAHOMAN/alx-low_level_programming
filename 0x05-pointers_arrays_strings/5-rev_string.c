eeb198504cba5ec279ba466cb9b6bee21e34be2b
2NG CODER <nahomnigussie7@gmail.com>
2022-12-20 13:57:30 -0800

    A good engineer thinks in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,7 +4,7 @@
  * @s: String to reverse
  * Return: nothing.
  */
-void rev_string(char *s)                                                                                                              
+void rev_string(char *s)
 {
 	int i = 0;
 	int aux = 0;
