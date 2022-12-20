55ed9afdda1c9bf8b5fc9b6c8a2392640fdf7584
2NG CODER <nahomnigussie7@gmail.com>
2022-12-20 06:05:45 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+/**
+ * strlen - find length of a string
+ * @s: pointer to the string to check
+ * Return: void
+ */
+int _strlen(char *s);
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
