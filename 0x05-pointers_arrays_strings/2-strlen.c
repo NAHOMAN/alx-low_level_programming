de012d5113cadfd5d1b3e0ae255c3c1b9efdf99d
2NG CODER <nahomnigussie7@gmail.com>
2022-12-20 06:37:13 -0800

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,10 +3,9 @@
  * @s: pointer to the string to check
  * Return: void
  */
-
 int _strlen(char *s)
 {
 	int index;
 	for (index = 0; s[index] != '\0'; index ++)
-		return (index);
+	return (index);
 }
