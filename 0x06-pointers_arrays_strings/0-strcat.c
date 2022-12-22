9b4722aad33506db123f9f014d82382f7b03d15b
2NG CODER <nahomnigussie7@gmail.com>
2022-12-22 11:56:58 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * strcat - the string pointed to by @src.include the terminate null byte to the end string pointed by dest.
+ * strcat - the string pointed to by @src.
+ * include the terminate null byte to the end string pointed by dest.
  * @dest: a pointer to string
  * @src: the source string to be appended to @dest.
  * Return: a pointer to destination string @dest.
  */
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
-	
-	while(dest[index++])
+	int index = 0,
+	    dest_len = 0;
+
+	while (dest[index++])
 		dest_len++;
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
