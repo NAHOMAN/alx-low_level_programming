10f111565e3d1b28d07bd7a0d340d8ece70e0edb
2NG CODER <nahomnigussie7@gmail.com>
2023-01-04 14:50:02 -0800

    They say we only use a fraction of our brain

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+int find _strlen(*s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+/**
+ * find _strlen - return thr length of string
+ * @s: string to be measured
+ * Return: the length of string
+ */
+int find _strlen(*s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		return (0);
+	}
+
+	return (len);
+}
+
+			
