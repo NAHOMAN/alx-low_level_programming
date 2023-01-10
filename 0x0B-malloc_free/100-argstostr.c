d8b9165256f2d45829b9426d4893de07f973e503
2NG CODER <nahomnigussie7@gmail.com>
2023-01-10 13:42:02 -0800

    is int the mountains ahead to climb that wear you out;

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,11 +9,11 @@
 char *argstostr(int ac, char **av)
 {
 	char *str;
-	int count = 0, a - 0, b - 0, c - 0;
+	int count = 0, a = 0, b = 0, c = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	while (a < ac)
+	while (av < ac)
 	{
 		b = 0;
 		while (av[a][b] != '\0')
