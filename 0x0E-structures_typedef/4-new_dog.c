04a003b1eb7f9117bf56cdff4002123e6fdb0434
2NG CODER <nahomnigussie7@gmail.com>
2023-01-17 12:18:54 -0800

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -31,14 +31,15 @@ char *_strcpy(char *dest, char *src)
 	int len, i;
 
 	len = 0;
-	while (src[len] != '\0')
-		{
-			len++;
-		}
-	for (i = 0; i < len; i++)
-		{
-			dest[i] = src[i];
-		}
+	while (len[src] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len;)
+	{
+		dest[i] = src[i];
+		i++;
+	}
 	dest[i] = '\0';
 	return (dest);
 }
