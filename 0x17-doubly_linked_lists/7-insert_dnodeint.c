2a750bcf90e0b91b1a697bf66d5ae33fff998981
2NG CODER <nahomnigussie7@gmail.com>
2023-03-23 05:15:24 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -39,3 +39,50 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	return (NULL);
 }
+/**
+ * *add_dnodeint - adds a new node at the beginning of a linked list
+ * @head: pointer to the first node in the list
+ * @n: data to insert in that new node
+ *
+ * Return: pointer to the new node, or NULL if it fails
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+        dlistint_t *new;
+
+        new = malloc(sizeof(dlistint_t));
+
+        if (!new)
+                return (NULL);
+        new->n = n;
+        new->next = *head;
+        *head = new;
+        return (new);
+}
+/**
+ * *add_dnodeint_end - adds a node at the end of a linked list
+ * @head: pointer to the first element in the list
+ * @n: data to insert in the new element
+ *
+ * Return: pointer to the new node, or NULL if it fails
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+        dlistint_t *temp = *head;
+
+        new = malloc(sizeof(dlistint_t));
+        if (!new)
+                return (NULL);
+        new->n = n;
+        new->next = NULL;
+        if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+	return (new);
+}
